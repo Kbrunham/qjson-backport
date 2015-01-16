7eda4c92651ddf5a1b36cb90c41bdb6ec9c00685
Kalen Brunham <kalen@brunham.net>
2015-01-15 23:50:11 -0500

    Initial version

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+
+#include "qjsondocument.h"
+#include "qjsonarray.h"
+#include "qjsonobject.h"
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
